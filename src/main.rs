80529ef01efe6ee1788a4ceaaf98ba8d57560cc5
Tristan Phease <tristan.phease@gmail.com>
2025-10-06 17:18:49 +1300

    Add times argument so I can sample more accurately

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,26 @@ fn main() {
     let file = fs::read(&args.file_in)
         .expect("Couldn't find file to be decoded");
     match args.decoder {
-        DecoderType::ImageWebP => decode_image_webp(&file),
-        DecoderType::LibWebP => decode_image_libwebp(&file),
+        DecoderType::ImageWebP => decode_image_webp(&file, args.times),
+        DecoderType::LibWebP => decode_image_libwebp(&file, args.times),
     }
 }
 
-fn decode_image_webp(file: &[u8]) {
-    let cursor = Cursor::new(file);
-    let mut decoder = image_webp::WebPDecoder::new(cursor)
-        .expect("Invalid webp file");
-    let mut buffer = vec![0u8; decoder.output_buffer_size().unwrap()];
-    decoder.read_image(&mut buffer).unwrap();
+fn decode_image_webp(file: &[u8], times: u32) {
+    for _ in 0..times {
+        let cursor = Cursor::new(file);
+        let mut decoder = image_webp::WebPDecoder::new(cursor)
+            .expect("Invalid webp file");
+        let mut buffer = vec![0u8; decoder.output_buffer_size().unwrap()];
+        decoder.read_image(&mut buffer).unwrap();
+    }
 }
 
-fn decode_image_libwebp(file: &[u8]) {
-    let decoder = webp::Decoder::new(file);
-    let _ = decoder.decode().unwrap();
+fn decode_image_libwebp(file: &[u8], times: u32) {
+    for _ in 0..times {
+        let decoder = webp::Decoder::new(file);
+        let _ = decoder.decode().unwrap();
+    }
 }
 
 #[derive(Parser, Debug)]
@@ -33,7 +37,10 @@ struct Args {
     file_in: String,
 
     #[arg(short, long, default_value = "ImageWebP")]
-    decoder: DecoderType
+    decoder: DecoderType,
+
+    #[arg(short, long, default_value = "1")]
+    times: u32,
 }
 
 #[derive(Clone, Copy, Debug, ValueEnum)]
